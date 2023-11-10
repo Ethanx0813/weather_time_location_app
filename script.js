f5e1e1f95a9a5e0b6715ad925ddb52c8dd78c050
Ethanx0813 <ankityadav770587@gmail.com>
2023-11-10 11:49:32 +0530

    weather app

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const button =document.getElementById("search-button");
-const input = document.getElementById("city-input");
-const cityName = document.getElementById('city-name');
-const cityTime = document.getElementById('city-time');
-const cityTemp = document.getElementById('city-temp');
-async function getData(cityName) {
-    const promise= await fetch(`http://api.weatherapi.com/v1/current.json?key=5fdc3024bfb04f95b04120512230110&q=${cityName}&aqi=no`
-    );
-return await promise.json()
-}
-button.addEventListener('click',async() => {
-    const value = input.value;
-    const result = await getData(value);
-    console.log(result)
-    cityName.innerText=`${result.location.name},${result.location.region},${result.location.country}`;
-    cityTime.innerText=result.location.localtime;
-    cityTemp.innerText=result.current.temp_c;
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const cityInput = document.getElementById("city-input");
+    const searchButton = document.getElementById("search-button");
+    const cityNameElement = document.getElementById("city-name");
+    const cityTimeElement = document.getElementById("city-time");
+    const cityTempElement = document.getElementById("city-temp");
+
+    async function getWeatherData(cityName) {
+        try {
+            const apiKey = '5fdc3024bfb04f95b04120512230110';
+            const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`);
+            const result = await response.json();
+
+            cityNameElement.textContent = `City: ${result.location.name}, ${result.location.country}`;
+            cityTimeElement.textContent = `Local Time: ${result.location.localtime}`;
+            cityTempElement.textContent = `Temperature: ${result.current.temp_c}°C`;
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
+        }
+    }
+
+    function searchCity() {
+        const cityName = cityInput.value.trim();
+
+        if (cityName !== "") {
+            getWeatherData(cityName);
+        } else {
+            alert("Please enter a city name.");
+        }
+    }
+
+    searchButton.addEventListener('click', searchCity);
+});
